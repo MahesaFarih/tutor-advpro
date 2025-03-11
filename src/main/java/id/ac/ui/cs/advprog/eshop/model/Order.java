9861b3f159d750dc66f4b800de6c04dd532b917e
MahesaFarih <mahesafarih7@gmail.com>
2025-03-11 22:58:09 +0700

    [GREEN] Implement Order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,25 +1,50 @@
 package id.ac.ui.cs.advprog.eshop.model;
+
 import lombok.Getter;
-import lombok.Setter;
 import lombok.Builder;
 
 import java.util.List;
+import java.util.Arrays;
 
 @Builder
 @Getter
 public class Order {
-    String id;
-    List<Product> products;
-    Long orderTime;
-    String author;
-    @Setter
-    String status;
+    private String id;
+    private List<Product> products;
+    private Long orderTime;
+    private String author;
+    private String status;
 
-    public Order(String id, List<Product> products, Long orderTime, String author){
+    public Order(String id, List<Product> products, Long orderTime, String author) {
+        this.id = id;
+        this.orderTime = orderTime;
+        this.author = author;
+        this.status = "WAITING_PAYMENT";
+
+        if (products.isEmpty()) {
+            throw new IllegalArgumentException("Products list cannot be empty");
+        } else {
+            this.products = products;
+        }
     }
 
-    public Order(String id, List<Product> products, Long orderTime, String author, String status){
+    public Order(String id, List<Product> products, Long orderTime, String author, String status) {
+        this(id, products, orderTime, author);
 
+        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
+        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
+            throw new IllegalArgumentException("Invalid status");
+        } else {
+            this.status = status;
+        }
     }
 
+    public void setStatus(String status) {
+        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
+        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
+            throw new IllegalArgumentException("Invalid status");
+        } else {
+            this.status = status;
+        }
+    }
 }
