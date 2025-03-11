ea32b11996c87c736cb064319797f17ad26fc62d
MahesaFarih <mahesafarih7@gmail.com>
2025-03-11 22:13:11 +0700

    Add tests for Order model.

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -79,4 +79,5 @@ public class OrderTest {
                 this.products, 1708560000L, "Safira Sudrajat");
         assertThrows(IllegalArgumentException.class, () -> order.setStatus("MEOW"));
     }
+
 }
